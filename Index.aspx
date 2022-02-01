28acb47b4df352588c8c3a2c0e4ea70d75d6aeb7
Gladministrator <81259608+Gladministrator@users.noreply.github.com>
2022-01-31 22:00:04 -0500

    Initial Upload

---
+++ Index.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Pay_Application.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Styles/Styles.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="main-app-container">
+            <h1 style="color: #000080; font-size: 50px; font-weight: bold; font-style: normal; text-transform: none; font-family: Arial, Helvetica, sans-serif;">Pay Calculator</h1>
+            <div class="all-inputs-container">
+                <div class="individual-input">
+                    <asp:Label ID="FirstNameLabel" runat="server" Text="First Name:"></asp:Label>
+                    <div>
+                        <asp:TextBox ID="FirstNameText" runat="server"></asp:TextBox>
+                        <asp:Label ID="InfoFirstNameLabel" runat="server" Text="*" ToolTip="Enter Your First Name" Visible="False" ForeColor="Red"></asp:Label>
+                    </div>
+                </div>
+                <div class="individual-input">
+                    <asp:Label ID="LastNameLabel" runat="server" Text="Last Name:"></asp:Label>
+                    <div>
+                        <asp:TextBox ID="LastNameText" runat="server"></asp:TextBox>
+                        <asp:Label ID="InfoLastNameLabel" runat="server" Text="*" ToolTip="Enter Your Last Name" Visible="false" ForeColor="Red"></asp:Label>
+                    </div>
+                </div>
+                <div class="individual-input">
+                    <asp:Label ID="HoursLabel" runat="server" Text="Hours Worked:"></asp:Label>
+                    <div>
+                        <asp:TextBox ID="HoursText" runat="server"></asp:TextBox>
+                        <asp:Label ID="InfoHoursLabel" runat="server" Text="*" ToolTip="Enter Hours Worked" Visible="false" ForeColor="Red"></asp:Label>
+                    </div>
+                </div>
+                <div class="individual-input">
+                    <asp:Label ID="HourlyPayLabel" runat="server" Text="Hourly Pay:"></asp:Label>
+                    <div>
+                        <asp:TextBox ID="HourlyPayText" runat="server"></asp:TextBox>
+                        <asp:Label ID="InfoHourlyPay" runat="server" Text="*" ToolTip="Enter Hourly Pay" Visible="false" ForeColor="Red"></asp:Label>
+                    </div>
+                </div>
+                <asp:Label ID="Error" runat="server" Text="Please review fields marked with an asterisk * " ForeColor="Black" Font-Size="21px" Visible="false"></asp:Label>
+            </div>
+            <asp:Button ID="Button1" CssClass="calculate-button" runat="server" Text="Calculate Pay Statement" OnClick="Button1_Click" />
+            <asp:Panel ID="PanelPayStatement" runat="server" Visible="false" CssClass="payment-panel">
+                <h2>Pay Statement</h2>
+                <div>
+                    <asp:Label ID="PayNameLabel" runat="server" Text="Name:"></asp:Label>
+                    <asp:TextBox ID="PayNameText" runat="server"  ReadOnly="true" Width='280px'></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayTotalHoursWorkedLabel" runat="server" Text="Total Hours Worked:"></asp:Label>
+                    <asp:TextBox ID="PayTotalHoursWorkedText" runat="server" ReadOnly="true" Width='280px'></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayHourlyRateLabel" runat="server" Text="Hourly Pay Rate:"></asp:Label>
+                    <asp:TextBox ID="PayHourlyRateText" runat="server" ReadOnly="true" Width='280px'></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayOvertimeHoursLabel" runat="server" Text="Overtime Hours:"></asp:Label>
+                    <asp:TextBox ID="PayOvertimeHoursText" runat="server" ReadOnly="true" Width="280px">0</asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayRegularPayLabel" runat="server" Text="Regular Pay:"></asp:Label>
+                    <asp:TextBox ID="PayRegularPayText" runat="server" ReadOnly="true" Width='280px'></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayOvertimePayLabel" runat="server" Text="Overtime Pay:"></asp:Label>
+                    <asp:TextBox ID="PayOvertimePayText" runat="server" ReadOnly="true" Width='280px'>0</asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayTotalGrossPayLabel" runat="server" Text="Total Gross Pay:"></asp:Label>
+                    <asp:TextBox ID="PayTotalGrossPayText" runat="server" ReadOnly="true" Width='280px'>0</asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayTaxesLabel" runat="server" Text="Taxes:"></asp:Label>
+                    <asp:TextBox ID="PayTaxesText" runat="server" ReadOnly="true" Width='280px'></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="PayNetPayLabel" runat="server" Text="Net Pay:"></asp:Label>
+                    <asp:TextBox ID="PayNetPayText" runat="server" ReadOnly="true" Width="280px"></asp:TextBox>
+                </div>
+                <asp:Button ID="ClearButton" CssClass="clear-button" runat="server" Text="CLEAR" OnClick="ClearButton_Click" />
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace Pay_Application
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        /// page load visibility changes so the pay statement is reset on re render.
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PanelPayStatement.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (
+            Filter_and_Add_Name(FirstNameText, InfoFirstNameLabel) &
+            Filter_and_Add_Name(LastNameText, InfoLastNameLabel)&
+            Validate_Number(HourlyPayText, InfoHourlyPay) &
+            Validate_Number(HoursText, InfoHoursLabel,80))
+            {
+                Error.Visible = false;
+                PayNameText.Text = FirstNameText.Text + " " + LastNameText.Text;
+                PayHourlyRateText.Text = HourlyPayText.Text;
+                PayTotalHoursWorkedText.Text = HoursText.Text;
+                Compensation_Calcs(HoursText, HourlyPayText);
+                PanelPayStatement.Visible = true;
+            }
+            else
+            {
+                Error.Visible = true;
+            }
+        }
+        /// <summary>
+        /// calculates compensation based on pay and 40 hour threshold for Overtime. 24% static tax bracket
+        /// is used here. 
+        /// Calculates regular pay, overtime pay, taxes, total pay, and total pay after taxes.
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="payRate"></param>
+        void Compensation_Calcs(TextBox hours,TextBox payRate)
+        {
+            decimal dTotalPay;
+            decimal dRegularPay;
+            decimal dTaxes;
+            decimal dTotalHours = Convert.ToDecimal(hours.Text);
+            decimal dPayRate = Convert.ToDecimal(payRate.Text);
+            if (dTotalHours > 40M)
+            {
+                decimal dOvertimeHours = dTotalHours - 40M;
+                PayOvertimeHoursText.Text = dOvertimeHours.ToString("F");
+
+                decimal dOvertimePay = dOvertimeHours * dPayRate * 1.5M;
+                PayOvertimePayText.Text = dOvertimePay.ToString("F");
+
+                dRegularPay = 40M * dPayRate;
+
+                dTotalPay = dRegularPay + dOvertimePay;
+            }
+            else
+            {
+                dRegularPay = dTotalHours * dPayRate;
+                PayRegularPayText.Text = dRegularPay.ToString("F");
+                dTotalPay = dRegularPay;
+
+            }
+            PayRegularPayText.Text = dRegularPay.ToString("F");
+            PayTotalGrossPayText.Text = dTotalPay.ToString("F");
+            dTaxes = dTotalPay * 0.24M;
+            PayTaxesText.Text = dTaxes.ToString("F");
+            PayNetPayText.Text = (dTotalPay - dTaxes).ToString("F"); 
+        }
+        /// <summary>
+        /// validats number inputs to be greater than 0.
+        /// </summary>
+        /// <param name="numberData">number text to check</param>
+        /// <param name="infoLabelTip">tooltip to adjust if needed with applicable messages</param>
+        /// <returns></returns>
+        bool Validate_Number(TextBox numberData, Label infoLabelTip, decimal limit = decimal.MaxValue)
+        {
+            try
+            {
+                decimal dNumber = Convert.ToDecimal(numberData.Text);
+
+                if (dNumber <= 0M || dNumber>limit)
+                {
+                    infoLabelTip.Visible = true;
+                    infoLabelTip.ToolTip = dNumber <= 0M ? "Please input a value greater than 0." : "Value can not exceed 80";
+                    return false;
+                }
+                numberData.Text = dNumber.ToString("F");
+                infoLabelTip.Visible = false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                infoLabelTip.Visible = true;
+                infoLabelTip.ToolTip = ex.Message + " " + "Please input a numeric value in this field. Decimal values are also accepted.";
+                return false;
+            }
+
+        }
+        /// <summary>
+        /// Method to filter the text input leaving only Alphabet characters and hyphens (-) as they can be common in names.
+        /// Regex will check to ensure only valid characters are entered.
+        /// </summary>
+        /// <param name="inputTextData">textbox field to check</param>
+        /// <param name="infoLabelTip">label to modify if invalid</param>
+        /// <returns></returns>
+        bool Filter_and_Add_Name (TextBox inputTextData, Label infoLabelTip)
+        {
+            string filteredString = Regex.Replace(inputTextData.Text , @"[^A-Za-z\-]", "");
+            inputTextData.Text = filteredString.Trim();
+
+            if (filteredString.Trim().Length > 0)
+            {
+                infoLabelTip.Visible = false;
+                return true;
+            }
+            else
+            {
+                infoLabelTip.Visible = true;
+                infoLabelTip.ToolTip = "Please enter valid English Alphabet Characters. Hyphens are accepted.";
+                return false;
+            }
+
+        }
+
+        protected void ClearButton_Click(object sender, EventArgs e)
+        {
+            FirstNameText.Text = String.Empty;
+            LastNameText.Text = String.Empty;
+            HourlyPayText.Text = String.Empty;
+            HoursText.Text = String.Empty;
+
+        }
+    }
+}
