628726dba8bd0efd7ae3df6bdc4478f4faed00ef
Gladministrator <Potato_Pat3000@Outlook.com>
2026-03-15 15:31:36 -0400

    updated

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -1,28 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
 
 namespace Pay_Application
 {
     public partial class Index : System.Web.UI.Page
     {
         /// page load visibility changes so the pay statement is reset on re render.
-        protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load (object sender, EventArgs e)
         {
             PanelPayStatement.Visible = false;
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
+        protected void Button1_Click (object sender, EventArgs e)
         {
             if (
             Filter_and_Add_Name(FirstNameText, InfoFirstNameLabel) &
-            Filter_and_Add_Name(LastNameText, InfoLastNameLabel)&
+            Filter_and_Add_Name(LastNameText, InfoLastNameLabel) &
             Validate_Number(HourlyPayText, InfoHourlyPay) &
-            Validate_Number(HoursText, InfoHoursLabel,80))
+            Validate_Number(HoursText, InfoHoursLabel, 80))
             {
                 Error.Visible = false;
                 PayNameText.Text = FirstNameText.Text + " " + LastNameText.Text;
@@ -36,6 +32,7 @@ namespace Pay_Application
                 Error.Visible = true;
             }
         }
+
         /// <summary>
         /// calculates compensation based on pay and 40 hour threshold for Overtime. 24% static tax bracket
         /// is used here. 
@@ -43,19 +40,19 @@ namespace Pay_Application
         /// </summary>
         /// <param name="hours"></param>
         /// <param name="payRate"></param>
-        void Compensation_Calcs(TextBox hours,TextBox payRate)
+        private void Compensation_Calcs (TextBox hours, TextBox payRate)
         {
             decimal dTotalPay;
             decimal dRegularPay;
             decimal dTaxes;
-            decimal dTotalHours = Convert.ToDecimal(hours.Text);
-            decimal dPayRate = Convert.ToDecimal(payRate.Text);
+            var dTotalHours = Convert.ToDecimal(hours.Text);
+            var dPayRate = Convert.ToDecimal(payRate.Text);
             if (dTotalHours > 40M)
             {
-                decimal dOvertimeHours = dTotalHours - 40M;
+                var dOvertimeHours = dTotalHours - 40M;
                 PayOvertimeHoursText.Text = dOvertimeHours.ToString("F");
 
-                decimal dOvertimePay = dOvertimeHours * dPayRate * 1.5M;
+                var dOvertimePay = dOvertimeHours * dPayRate * 1.5M;
                 PayOvertimePayText.Text = dOvertimePay.ToString("F");
 
                 dRegularPay = 40M * dPayRate;
@@ -73,21 +70,22 @@ namespace Pay_Application
             PayTotalGrossPayText.Text = dTotalPay.ToString("F");
             dTaxes = dTotalPay * 0.24M;
             PayTaxesText.Text = dTaxes.ToString("F");
-            PayNetPayText.Text = (dTotalPay - dTaxes).ToString("F"); 
+            PayNetPayText.Text = (dTotalPay - dTaxes).ToString("F");
         }
+
         /// <summary>
         /// validats number inputs to be greater than 0.
         /// </summary>
         /// <param name="numberData">number text to check</param>
         /// <param name="infoLabelTip">tooltip to adjust if needed with applicable messages</param>
         /// <returns></returns>
-        bool Validate_Number(TextBox numberData, Label infoLabelTip, decimal limit = decimal.MaxValue)
+        private bool Validate_Number (TextBox numberData, Label infoLabelTip, decimal limit = decimal.MaxValue)
         {
             try
             {
-                decimal dNumber = Convert.ToDecimal(numberData.Text);
+                var dNumber = Convert.ToDecimal(numberData.Text);
 
-                if (dNumber <= 0M || dNumber>limit)
+                if (dNumber <= 0M || dNumber > limit)
                 {
                     infoLabelTip.Visible = true;
                     infoLabelTip.ToolTip = dNumber <= 0M ? "Please input a value greater than 0." : "Value can not exceed 80";
@@ -103,8 +101,8 @@ namespace Pay_Application
                 infoLabelTip.ToolTip = ex.Message + " " + "Please input a numeric value in this field. Decimal values are also accepted.";
                 return false;
             }
-
         }
+
         /// <summary>
         /// Method to filter the text input leaving only Alphabet characters and hyphens (-) as they can be common in names.
         /// Regex will check to ensure only valid characters are entered.
@@ -112,9 +110,9 @@ namespace Pay_Application
         /// <param name="inputTextData">textbox field to check</param>
         /// <param name="infoLabelTip">label to modify if invalid</param>
         /// <returns></returns>
-        bool Filter_and_Add_Name (TextBox inputTextData, Label infoLabelTip)
+        private bool Filter_and_Add_Name (TextBox inputTextData, Label infoLabelTip)
         {
-            string filteredString = Regex.Replace(inputTextData.Text , @"[^A-Za-z\-]", "");
+            var filteredString = Regex.Replace(inputTextData.Text, @"[^A-Za-z\-]", "");
             inputTextData.Text = filteredString.Trim();
 
             if (filteredString.Trim().Length > 0)
@@ -128,15 +126,14 @@ namespace Pay_Application
                 infoLabelTip.ToolTip = "Please enter valid English Alphabet Characters. Hyphens are accepted.";
                 return false;
             }
-
         }
 
-        protected void ClearButton_Click(object sender, EventArgs e)
+        protected void ClearButton_Click (object sender, EventArgs e)
         {
-            FirstNameText.Text = String.Empty;
-            LastNameText.Text = String.Empty;
-            HourlyPayText.Text = String.Empty;
-            HoursText.Text = String.Empty;
+            FirstNameText.Text = string.Empty;
+            LastNameText.Text = string.Empty;
+            HourlyPayText.Text = string.Empty;
+            HoursText.Text = string.Empty;
 
         }
     }
